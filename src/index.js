a7e73d611b69641d5f188f08a75ced5fb0a080cf
Swarup Das <78835981+swarupcs@users.noreply.github.com>
2024-12-15 20:46:52 +0530

    server setup

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import express from "express";
+import dotenv from "dotenv"
+import connectDB from "./database/db.js";
+
+dotenv.config({})
+
+// call database connection
+connectDB();
 
 const app = express();
 
-const PORT = 8080;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
     console.log(`Server listen at ${PORT}`)
